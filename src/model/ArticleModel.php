1a002ba8cb6da2f27885a5af711b3d2f5da99a84
blaj <blaj.kontakt@gmail.com>
2019-03-11 23:39:22 +0100

    delete unused code

--- src/model/ArticleModel.php
+++ src/model/ArticleModel.php
@@ -42,26 +42,4 @@ class ArticleModel extends Model implements IModel
     {
         $this->content = $content;
     }
-
-    // public function getAll()
-    // {
-    //     $query = $this->db->query("SELECT * FROM article");
-    //     $items = $query->fetchAll(\PDO::FETCH_ASSOC);
-    //     if (isset($items))
-    //         return $items;
-    //     else
-    //         return null;
-    // }
-    //
-    // public function getOne($id)
-    // {
-    //     $query = $this->db->prepare("SELECT * FROM article WHERE id = :id");
-    //     $query->execute(['id' => $id]);
-    //     $items = $query->fetchAll(\PDO::FETCH_ASSOC);
-    //     if (isset($items[0])) {
-    //         return $items[0];
-    //     } else {
-    //         return null;
-    //     }
-    // }
 }
